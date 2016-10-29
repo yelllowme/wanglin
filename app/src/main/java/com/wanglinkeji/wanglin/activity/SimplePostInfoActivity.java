acaae70b21ebbd6605c0a96e62e717092270ed55
w1056695125 <2271581194@qq.com>
2016-10-29 19:05:17 +0800

    添加报修boos直通建议功能

---
+++ app/src/main/java/com/wanglinkeji/wanglin/activity/SimplePostInfoActivity.java
@@ -0,0 +1,13 @@
+package com.wanglinkeji.wanglin.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SimplePostInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_post_info);
+    }
+}
