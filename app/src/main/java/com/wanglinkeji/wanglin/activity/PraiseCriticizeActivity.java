1623bed47e8e7223495af6bed4e1490a5a54045f
w1056695125 <2271581194@qq.com>
2016-11-12 11:59:44 +0800

    Changes to be committed: 	new file:   app/src/main/java/com/wanglinkeji/wanglin/activity/PraiseCriticizeActivity.java 	new file:   app/src/main/java/com/wanglinkeji/wanglin/customerview/LeftRightTxtPrecentView.java 	new file:   app/src/main/java/com/wanglinkeji/wanglin/customerview/MaterialDialog.java 	new file:   app/src/main/res/drawable/bg_button_normal.xml 	new file:   app/src/main/res/drawable/bg_button_pressed.xml 	new file:   app/src/main/res/drawable/button.xml

---
+++ app/src/main/java/com/wanglinkeji/wanglin/activity/PraiseCriticizeActivity.java
@@ -0,0 +1,13 @@
+package com.wanglinkeji.wanglin.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PraiseCriticizeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_praise_criticize);
+    }
+}
---
+++ app/src/main/java/com/wanglinkeji/wanglin/customerview/MaterialDialog.java
@@ -0,0 +1,8 @@
+package com.wanglinkeji.wanglin.customerview;
+
+/**
+ * Created by jack on 2016/11/8.
+ */
+
+public class MaterialDialog {
+}
